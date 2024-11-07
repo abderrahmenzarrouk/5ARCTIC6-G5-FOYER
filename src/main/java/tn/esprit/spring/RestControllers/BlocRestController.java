e29f62af18cdd570cd32a2364e65c5b3da6dfab1
abderrahmenzarrouk <124931486+abderrahmenzarrouk@users.noreply.github.com>
2024-11-07 08:25:15 +0100

    Update BlocRestController.java

--- src/main/java/tn/esprit/spring/RestControllers/BlocRestController.java
+++ src/main/java/tn/esprit/spring/RestControllers/BlocRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class BlocRestController {
     IBlocService service;
 
-    @PostMapping("addOrUpdate")
+    @PostMapping("addOrUpdatee")
     Bloc addOrUpdate(@RequestBody Bloc b) {
         return service.addOrUpdate(b);
     }
